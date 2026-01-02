702588a6fa2bb9ae2b4015f46be50a7fedb7ab58
ccd2s <tasaedem@outlook.com>
2026-01-02 22:40:02 +0800

    fix: 修改代码使其可用

--- InfiniteAmmoMain.cs
+++ InfiniteAmmoMain.cs
@@ -3,43 +3,62 @@ using LabApi.Features;
 using LabApi.Loader.Features.Plugins;
 using System;
 
-namespace LabAPI_InfiniteAmmo
+namespace LabAPI_InfiniteAmmo;
+
+/// <summary>
+/// 无限子弹 插件。
+/// </summary>
+public class InfiniteAmmoMain : Plugin
 {
     /// <summary>
-    /// 无限子弹 插件。
+    /// 单例模式。
     /// </summary>
-    public class InfiniteAmmoMain : Plugin
+    public static InfiniteAmmoMain Singleton { get; private set; }
+
+    /// <summary>
+    /// 插件名称。
+    /// </summary>
+    public override string Name => "Infinite Ammo Plugin";
+    /// <summary>
+    /// 插件描述。
+    /// </summary>
+    public override string Description => "Grant players infinite ammunition";
+    /// <summary>
+    /// 插件作者。
+    /// </summary>
+    public override string Author => "TASA-Ed Studio";
+    /// <summary>
+    /// 插件版本。
+    /// </summary>
+    public override Version Version => new (1, 0, 0, 0);
+
+    /// <summary>
+    /// 需要的 LabApi 版本。
+    /// </summary>
+    public override Version RequiredApiVersion => new (LabApiProperties.CompiledVersion);
+
+    /// <summary>
+    /// 自定义事件处理器。
+    /// </summary>
+    public InfiniteAmmoEvent Events { get; private set; }
+
+    // 启用插件。
+    public override void Enable()
     {
-        /// <summary>
-        /// 插件信息。
-        /// </summary>
-        public override string Name => "Infinite Ammo Plugin";
-        public override string Description => "Give Player Infinite Ammo";
-        public override string Author => "TASA-Ed Studio";
+        Singleton = this;
 
-        /// <summary>
-        /// 插件版本。
-        /// </summary>
-        public override Version Version => new (0, 1, 0, 0);
+        Events = new InfiniteAmmoEvent();
 
-        /// <summary>
-        /// 需要的 LabApi 版本。
-        /// </summary>
-        public override Version RequiredApiVersion => new (LabApiProperties.CompiledVersion);
+        CustomHandlersManager.RegisterEventsHandler(Events);
+    }
 
-        /// <summary>
-        /// Events handler object.
-        /// </summary>
-        public InfiniteAmmoEvent Events { get; } = new ();
+    // 禁用插件。
+    public override void Disable()
+    {
+        CustomHandlersManager.UnregisterEventsHandler(Events);
 
-        public override void Enable()
-        {
-            CustomHandlersManager.RegisterEventsHandler(Events);
-        }
+        Events = null;
 
-        public override void Disable()
-        {
-            CustomHandlersManager.UnregisterEventsHandler(Events);
-        }
+        Singleton = null;
     }
 }
