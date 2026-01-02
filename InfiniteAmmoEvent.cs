1599f25a7a3b64d358676bc628a1d0f3d518ff20
ccd2s <tasaedem@outlook.com>
2026-01-02 19:51:11 +0800

    添加项目文件。

---
+++ InfiniteAmmoEvent.cs
@@ -0,0 +1,21 @@
+﻿using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.CustomHandlers;
+
+namespace LabAPI_InfiniteAmmo
+{
+    public class InfiniteAmmoEvent : CustomEventsHandler
+    {
+        public override void OnPlayerDryFiringWeapon(PlayerDryFiringWeaponEventArgs ev)
+        {
+            ev.Player.SetAmmo(ev.FirearmItem.AmmoType, (ushort)(ev.Player.GetAmmo(ev.FirearmItem.AmmoType) + 1));
+        }
+        /// <summary>
+        /// 禁止玩家掉落子弹
+        /// </summary>
+        /// <param name="ev">PlayerDroppingAmmoEventArgs</param>
+        public override void OnPlayerDroppingAmmo(PlayerDroppingAmmoEventArgs ev)
+        {
+            ev.IsAllowed = false;
+        }
+    }
+}
---
+++ InfiniteAmmoMain.cs
@@ -0,0 +1,45 @@
+﻿using LabApi.Events.CustomHandlers;
+using LabApi.Features;
+using LabApi.Loader.Features.Plugins;
+using System;
+
+namespace LabAPI_InfiniteAmmo
+{
+    /// <summary>
+    /// 无限子弹 插件。
+    /// </summary>
+    public class InfiniteAmmoMain : Plugin
+    {
+        /// <summary>
+        /// 插件信息。
+        /// </summary>
+        public override string Name => "Infinite Ammo Plugin";
+        public override string Description => "Give Player Infinite Ammo";
+        public override string Author => "TASA-Ed Studio";
+
+        /// <summary>
+        /// 插件版本。
+        /// </summary>
+        public override Version Version => new (0, 1, 0, 0);
+
+        /// <summary>
+        /// 需要的 LabApi 版本。
+        /// </summary>
+        public override Version RequiredApiVersion => new (LabApiProperties.CompiledVersion);
+
+        /// <summary>
+        /// Events handler object.
+        /// </summary>
+        public InfiniteAmmoEvent Events { get; } = new ();
+
+        public override void Enable()
+        {
+            CustomHandlersManager.RegisterEventsHandler(Events);
+        }
+
+        public override void Disable()
+        {
+            CustomHandlersManager.UnregisterEventsHandler(Events);
+        }
+    }
+}
